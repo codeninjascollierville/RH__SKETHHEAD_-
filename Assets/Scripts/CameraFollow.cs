4ff9e733038457223ec7a1d430835e847bc47fb7
codeninjascollierville <107887805+codeninjascollierville@users.noreply.github.com>
2022-10-27 18:11:31 -0500

    ytdnutrsnuiuykmotiuptopiupopuityiuydrstsrrbnrnr
    erthdnujgvmhvgmfijyrnhfw

--- Assets/Scripts/CameraFollow.cs
+++ Assets/Scripts/CameraFollow.cs
@@ -4,6 +4,8 @@ using UnityEngine;
 
 public class CameraFollow : MonoBehaviour
 {
+    [Header("Target Object")]
+    public Transform target;
     // Start is called before the first frame update
     void Start()
     {
@@ -11,8 +13,11 @@ public class CameraFollow : MonoBehaviour
     }
 
     // Update is called once per frame
-    void Update()
+    private void Update()
     {
-
+        if(target.position.y > transform.position.y)
+        {
+            transform.position = new Vector3(target.transform.position.x, target.transform.position.y, transform.position.z);
+        }
     }
 }
--- Assets/Scripts/PlayerControls.cs
+++ Assets/Scripts/PlayerControls.cs
@@ -5,15 +5,39 @@ using UnityEngine.UI;
 
 public class PlayerControls : MonoBehaviour
 {
+    [Header("Rigid Body")]
+    public Rigidbody2D rb;
+    [Header("Default Down Speed")]
+    public float downSpeed = 20f;
+    [Header("Default Movement Speed")]
+    public float movementSpeed = 10f;
+    [Header("Default directional Movement Speed")]
+    public float movement = 0f;
     // Start is called before the first frame update
     void Start()
     {
-
+        rb = GetComponent<Rigidbody2D>();
     }
 
     // Update is called once per frame
     void Update()
     {
+        movement = Input.GetAxis("Horizontal") * movementSpeed;
+        if (movement < 0)
+        {
+            this.GetComponent<SpriteRenderer>().flipX = true;
+        }
+    }
 
+    void FixedUpdate()
+    {
+        Vector2 velocity = rb.velocity;
+        velocity.x = movement;
+        rb.velocity = velocity;
+    }
+
+    private void OnCollisionEnter2D(Collision2D collision)
+    {
+        rb.velocity = new Vector3(rb.velocity.x, downSpeed, 0);
     }
 }
